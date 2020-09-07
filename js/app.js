032bad84e5bba31ebbfbcc69aa62387b437e1493
VictorFlorez <victor_florez82171@elpoli.edu.co>
2020-09-07 14:08:56 -0500

    Se leyeron los valores del formulario

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 // Variables
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+const datoBusqueda = {
+    marca,
+    year,
+    minimo,
+    maximo,
+    puertas,
+    transmision,
+    color
+};
 
 const resultado = document.querySelector('#resultado'); 
-const year = document.querySelector('#year');
+ 
 
 const max = new Date().getFullYear();
 const min = max - 10;
@@ -16,6 +33,49 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelect();
 });
 
+// EventsListener para los Select
+
+marca.addEventListener('change', (e) => {
+   // e.target.value: capturo la opcion del usuario
+   datoBusqueda.marca = e.target.value;
+   console.log(datoBusqueda);
+})
+
+year.addEventListener('change', (e) => {
+   
+    datoBusqueda.year = e.target.value;
+    console.log(datoBusqueda);
+ })
+
+ minimo.addEventListener('change', (e) => {
+   
+    datoBusqueda.minimo = e.target.value;
+    console.log(datoBusqueda);
+ })
+
+ maximo.addEventListener('change', (e) => {
+    
+    datoBusqueda.maximo = e.target.value;
+    console.log(datoBusqueda);
+ })
+
+ puertas.addEventListener('change', (e) => {
+    
+    datoBusqueda.puertas = e.target.value;
+    console.log(datoBusqueda);
+ })
+
+ transmision.addEventListener('change', (e) => {
+    
+    datoBusqueda.transmision = e.target.value;
+    console.log(datoBusqueda);
+ })
+
+ color.addEventListener('change', (e) => {
+    
+    datoBusqueda.color = e.target.value;
+    console.log(datoBusqueda);
+ })
 
 // Funciones
 
@@ -37,14 +97,10 @@ function mostrarAutos() {
 
 function llenarSelect(){
     
-    for(let i = max; i >= min ; i--){
-        
-        const yearHTML = document.createElement('option');
-        yearHTML.textContent = `
-            ${i}
-        `;
-        year.appendChild(yearHTML);
-    }
+    for(let i = max; i >= min ; i--){ // Recorrer desde el 2020 al 2010
 
-     
+        const yearHTML = document.createElement('option'); //Crear un elemento <option>
+        yearHTML.textContent = `${i}`; // Le asigno el valor de i
+        year.appendChild(yearHTML); // Lo inserto en el HTML
+    }
 }
